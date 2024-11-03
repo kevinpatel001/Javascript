efc96ccbc82cb7c22b359ff354b2b1ea2eeb9b00
Kevin Patel <patelkevin.4874@gmail.com>
2024-11-03 17:04:46 +0530

    practice of function
    practice of function with if else condition and parameters

---
+++ function/praticeQuestionForFunction/checkEvenOrOdd.js
@@ -0,0 +1,17 @@
+// Write a function called isEven that takes a number as an argument and returns true if the number is even and false if it’s odd.
+
+
+function evenOdd(number){
+
+if(number%2==0){
+    console.log(number,"is even number")
+    return
+
+}
+else(number%2!=0)
+console.log(number,"is odd number")
+
+
+}
+
+evenOdd(6)
---
+++ function/praticeQuestionForFunction/sumOfArrayInFunction.js
@@ -0,0 +1,14 @@
+// Write a function called sumArray that takes an array of numbers and returns the sum of all its elements.
+
+function sumArray() {
+
+
+    arry1 = [0, 1, 2, 3]
+
+    sum = 0
+    sum = arry1[0] + arry1[1] + arry1[2] + arry1[3];
+    console.log(sum)
+
+
+}
+sumArray()
